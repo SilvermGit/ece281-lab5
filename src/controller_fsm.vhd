9826b731f24643b1b8531dfd0ba7865b28fc2230
SilvermGit <c28javier.silverman-lord@afacademy.af.edu>
2026-04-30 16:36:41 -0600

    letssee

--- src/controller_fsm.vhd
+++ src/controller_fsm.vhd
@@ -38,8 +38,44 @@ entity controller_fsm is
 end controller_fsm;
 
 architecture FSM of controller_fsm is
-
+    type sm_controller is (s_controller1, s_controller2, s_controller3, s_controller4);
+    
+    signal f_Q, f_Q_next: sm_controller;
 begin
-
+    state_process: process(i_adv)
+    begin
+        if (rising_edge(i_adv)) then
+            if (i_reset = '1') then
+                f_Q <= s_controller1;
+            else 
+                case f_Q is
+                    when s_controller1 => 
+                        if (i_adv = '1') then f_Q_next <= s_controller2; end if;
+                        if (i_adv = '1') then f_Q <= s_controller2; end if;
+                    when s_controller2 => 
+                        if (i_adv = '1') then f_Q_next <= s_controller3; end if;
+                        if (i_adv = '1') then f_Q <= s_controller3; end if;
+                    when s_controller3 => 
+                        if (i_adv = '1') then f_Q_next <= s_controller4; end if;
+                        if (i_adv = '1') then f_Q <= s_controller4; end if;
+                    when s_controller4 => 
+                        if (i_adv = '1') then f_Q_next <= s_controller1; end if;
+                        if (i_adv = '1') then f_Q <= s_controller1; end if;     
+                end case;
+            end if;
+        end if;
+    end process;
+    
+    output_process: process (f_Q)
+    begin
+        case f_Q is
+            when s_controller1 => o_cycle <= "0001";
+            when s_controller2 => o_cycle <= "0010";
+            when s_controller3 => o_cycle <= "0100";
+            when s_controller4 => o_cycle <= "1000";
+        end case;
+    end process;            
+            
+           
 
 end FSM;
